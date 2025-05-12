61b346b2e5f586a2ab4cf786a8a7ae4f181554f7
Nikita Petrovets <lmikon007@gmail.com>
2025-05-12 22:01:11 +0300

    feat: implement task 8

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,34 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const list = ['head', 'second', 'last'];
+
+    const objList = {
+      value: 'head',
+      next: {
+        value: 'second',
+        next: {
+          value: 'last',
+          next: {
+            value: null,
+            next: null,
+          },
+        },
+      },
+    };
+
+    const result = generateLinkedList(list);
+    expect(result).toStrictEqual(objList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const input = [1, 2, 3];
+    const result = generateLinkedList(input);
+
+    expect(result).toMatchSnapshot();
   });
 });
